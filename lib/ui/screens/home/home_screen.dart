b877b65f177ae86e29360460ad331c44fc1ae1a6
BamBoo <64342840+jeongwoo0427@users.noreply.github.com>
2024-08-14 10:30:50 +0900

    fix : 성능문제로 인해 홈 화면 애니메이션 일부 제거 (마우스 인터렉티브)

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -37,23 +37,15 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   late final GifController _gifController;
-  late final AnimationController _aboutTextAnimationContrller;
-  late final AnimationController _kjwTextAnimationContrller;
   late final AnimationController _earthAnimationController;
 
-  final double _cardsMovemnetScale = 50;
-  final double _earthMovementScale = 30;
-  double _mousePositionX = 0; //(-1 ~ 1)
-  double _mousePositionY = 0;
 
   bool _isBusy = false;
 
   @override
   void initState() {
-    _gifController = GifController(vsync: this);
-    _aboutTextAnimationContrller = AnimationController(vsync: this);
-    _kjwTextAnimationContrller = AnimationController(vsync: this);
     _earthAnimationController = AnimationController(vsync: this);
+    _gifController = GifController(vsync: this);
     super.initState();
   }
 
@@ -63,174 +55,159 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
         ResponsiveValue<bool>(context, defaultValue: false, conditionalValues: [const Condition.smallerThan(name: TABLET, value: true)]).value;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: MouseRegion(
-        onHover: (event) {
-          //중앙을 피벗으로 둠
-          setState(() {
-            _mousePositionX = (event.position.dx / context.screenSize.width - 0.5) * 2; //(-1 ~ 1)
-            _mousePositionY = (event.position.dy / context.screenSize.height - 0.5) * 2;
-          });
-        },
-        child: SizedBox(
-            height: double.infinity,
-            width: double.infinity,
-            child: Stack(
-              children: [
-                AnimatedPositioned(
-                  curve: Curves.decelerate,
-                  duration: const Duration(milliseconds: 400),
-                  top: 30 - (_earthMovementScale * _mousePositionY),
-                  bottom: -30 + (_earthMovementScale * _mousePositionY),
-                  left: 0 - (_earthMovementScale * _mousePositionX),
-                  right: 0 + (_earthMovementScale * _mousePositionX),
-                  child: Padding(
-                    padding: EdgeInsets.all(context.getResponsiveValue(100, 10)),
-                    child: Gif(
-                      image: const AssetImage(AppAssets.BG_EARTH_ANIM),
-                      fit: BoxFit.contain,
-                      controller: _gifController,
-                      autostart: Autostart.loop,
-                      
-                      fps: 30,
-                      placeholder: (context) => const Center(child: Text('Loading ...')),
-                      onFetchCompleted: () {
-                        _gifController.reset();
-                        _gifController.forward();
-                        _earthAnimationController.forward();
-                      },
-                    ).animate(autoPlay: false, controller: _earthAnimationController, effects: [
-                      ScaleEffect(begin: Offset(0, 0), end: Offset(1, 1), duration: Duration(milliseconds: 1500), curve: Curves.decelerate),
-                    ]),
-                  ),
+      body: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Stack(
+            children: [
+              Positioned(
+                top: 30,
+                bottom: -30,
+                left: 0,
+                right: 0,
+                child: Padding(
+                  padding: EdgeInsets.all(context.getResponsiveValue(100, 10)),
+                  child: Gif(
+                    image: const AssetImage(AppAssets.BG_EARTH_ANIM),
+                    fit: BoxFit.contain,
+                    controller: _gifController,
+                    autostart: Autostart.loop,
+
+                    fps: 30,
+                    placeholder: (context) => const Center(child: Text('Loading ...')),
+                    onFetchCompleted: () {
+                      _gifController.reset();
+                      _gifController.forward();
+                      _earthAnimationController.forward();
+                    },
+                  ).animate(autoPlay: false, controller: _earthAnimationController, effects: [
+                    ScaleEffect(begin: Offset(0, 0), end: Offset(1, 1), duration: Duration(milliseconds: 1500), curve: Curves.decelerate),
+                  ]),
                 ),
-                AnimatedPositioned(
-                    curve: Curves.decelerate,
-                    duration: const Duration(milliseconds: 400),
-                    top: 0 - (_cardsMovemnetScale * _mousePositionY),
-                    bottom: 0 + (_cardsMovemnetScale * _mousePositionY),
-                    left: 0 - (_cardsMovemnetScale * _mousePositionX),
-                    right: 0 + (_cardsMovemnetScale * _mousePositionX),
-                    child: Container(
-                        width: double.infinity,
-                        height: double.infinity,
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            SizedBox(),
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                GlassySectorButton(
-                                  sectorTitle: Text('Who am I ?'),
-                                  icon: Icons.person,
-                                  isTablet: isTablet,
-                                  delayMs: 2000,
-                                  onTap: _isBusy
-                                      ? null
-                                      : () async {
-                                          HomeScreen.dynamicCurrentDirection = MovePageDirection.topLeft;
-                                          await context.pushNamed(IntroductionScreen.routeName);
-
-                                        },
-                                ),
-                                GlassySectorButton(
-                                  sectorTitle: Text('Skills'),
-                                  icon: Icons.electric_bolt,
-                                  isTablet: isTablet,
-                                  delayMs: 3000,
-                                  onTap: _isBusy
-                                      ? null
-                                      : () async {
-                                          HomeScreen.dynamicCurrentDirection = MovePageDirection.topRight;
-                                          await context.pushNamed(SkillsScreen.routeName);
-                                        },
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                GlassySectorButton(
-                                  sectorTitle: Text('Projects'),
-                                  icon: Icons.newspaper_outlined,
-                                  isTablet: isTablet,
-                                  delayMs: 2500,
-                                  onTap: _isBusy
-                                      ? null
-                                      : () async {
-                                          HomeScreen.dynamicCurrentDirection = MovePageDirection.bottomLeft;
-                                          await context.pushNamed(ProjectsScreen.routeName);
-                                        },
-                                ),
-                                GlassySectorButton(
-                                  sectorTitle: Text('Contact'),
-                                  icon: Icons.email,
-                                  isTablet: isTablet,
-                                  delayMs: 3500,
-                                  onTap: _isBusy
-                                      ? null
-                                      : () async {
-                                          HomeScreen.dynamicCurrentDirection = MovePageDirection.bottomRight;
-                                          await context.pushNamed(ContactScreen.routeName);
-                                        },
-                                ),
-                              ],
-                            ),
-                            SizedBox(),
-                          ],
-                        ))),
-
-                Positioned(
-                  bottom: 20,
-                    left: 0,
+              ),
+              Positioned(
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
                   right: 0,
-                  child: WatchWidget().animate().fadeIn(curve: Curves.decelerate,duration: 500.ms,delay: 4000.ms
-                  ),
-                ),
+                  child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          SizedBox(),
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              GlassySectorButton(
+                                sectorTitle: Text('Who am I ?'),
+                                icon: Icons.person,
+                                isTablet: isTablet,
+                                delayMs: 2000,
+                                onTap: _isBusy
+                                    ? null
+                                    : () async {
+                                        HomeScreen.dynamicCurrentDirection = MovePageDirection.topLeft;
+                                        await context.pushNamed(IntroductionScreen.routeName);
 
-                Positioned(
-                    left: 15,
-                    top: 15,
-                    child: Row(children: [
-                      Text(
-                        'About',
-                        style: TextStyle(
-                            color: context.colorScheme.onSurface.withOpacity(0.8),
-                            fontSize:
-                                context.getResponsiveValue<double>(30,20),
-                            fontWeight: FontWeight.w200),
-                      ).animate(
-                        controller: _aboutTextAnimationContrller,
+                                      },
+                              ),
+                              GlassySectorButton(
+                                sectorTitle: Text('Skills'),
+                                icon: Icons.electric_bolt,
+                                isTablet: isTablet,
+                                delayMs: 3000,
+                                onTap: _isBusy
+                                    ? null
+                                    : () async {
+                                        HomeScreen.dynamicCurrentDirection = MovePageDirection.topRight;
+                                        await context.pushNamed(SkillsScreen.routeName);
+                                      },
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              GlassySectorButton(
+                                sectorTitle: Text('Projects'),
+                                icon: Icons.newspaper_outlined,
+                                isTablet: isTablet,
+                                delayMs: 2500,
+                                onTap: _isBusy
+                                    ? null
+                                    : () async {
+                                        HomeScreen.dynamicCurrentDirection = MovePageDirection.bottomLeft;
+                                        await context.pushNamed(ProjectsScreen.routeName);
+                                      },
+                              ),
+                              GlassySectorButton(
+                                sectorTitle: Text('Contact'),
+                                icon: Icons.email,
+                                isTablet: isTablet,
+                                delayMs: 3500,
+                                onTap: _isBusy
+                                    ? null
+                                    : () async {
+                                        HomeScreen.dynamicCurrentDirection = MovePageDirection.bottomRight;
+                                        await context.pushNamed(ContactScreen.routeName);
+                                      },
+                              ),
+                            ],
+                          ),
+                          SizedBox(),
+                        ],
+                      ))),
+
+              Positioned(
+                bottom: 20,
+                  left: 0,
+                right: 0,
+                child: WatchWidget().animate().fadeIn(curve: Curves.decelerate,duration: 500.ms,delay: 4000.ms
+                ),
+              ),
+
+              Positioned(
+                  left: 15,
+                  top: 15,
+                  child: Row(children: [
+                    Text(
+                      'About',
+                      style: TextStyle(
+                          color: context.colorScheme.onSurface.withOpacity(0.8),
+                          fontSize:
+                              context.getResponsiveValue<double>(30,20),
+                          fontWeight: FontWeight.w200),
+                    ).animate(
+                      autoPlay: true,
+                      delay: Duration(milliseconds: 4500),
+                      effects: [
+                        BlurEffect(begin: Offset(100, 30), end: Offset(0, 0), duration: Duration(milliseconds: 1500)),
+                      ],
+                    ),
+                    Text(
+                      ' KJW',
+                      style: TextStyle(
+                          color: context.colorScheme.onSurface.withOpacity(0.8),
+                          fontSize:
+                              context.getResponsiveValue<double>(30,20),
+                          fontWeight: FontWeight.w500),
+                    ).animate(
                         autoPlay: true,
-                        delay: Duration(milliseconds: 4500),
+                        delay: Duration(milliseconds: 5000),
                         effects: [
                           BlurEffect(begin: Offset(100, 30), end: Offset(0, 0), duration: Duration(milliseconds: 1500)),
                         ],
-                      ),
-                      Text(
-                        ' KJW',
-                        style: TextStyle(
-                            color: context.colorScheme.onSurface.withOpacity(0.8),
-                            fontSize:
-                                context.getResponsiveValue<double>(30,20),
-                            fontWeight: FontWeight.w500),
-                      ).animate(
-                          controller: _kjwTextAnimationContrller,
-                          autoPlay: true,
-                          delay: Duration(milliseconds: 5000),
-                          effects: [
-                            BlurEffect(begin: Offset(100, 30), end: Offset(0, 0), duration: Duration(milliseconds: 1500)),
-                          ],
-                          onComplete: (_) {
-                            _setBusyState(false);
-                          })
-                    ]))
-              ],
-            )),
-      ),
+                        onComplete: (_) {
+                          _setBusyState(false);
+                        })
+                  ]))
+            ],
+          )),
     );
   }
 
