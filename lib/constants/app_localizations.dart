8fac0dbe1ba9981e2da0527add198b62aa6cc1d8
CodingThanos <64342840+jeongwoo0427@users.noreply.github.com>
2024-08-30 20:55:54 +0900

    fix : 자기소개 페이지 디자인 수정

--- lib/constants/app_localizations.dart
+++ lib/constants/app_localizations.dart
@@ -34,8 +34,8 @@ class KoreanLocalizations implements Localizations{
 
   @override
   // TODO: implement introduction_screen_letter_page_letter
-  String get introduction_screen_letter_page_letter => 'Flutter로 세상을 그려내는 개발자입니다.\n'
-      '코드 한 줄에 감성을 담아 iOS와 Android라는 두 개의 무대 위에 하나의 아름다운 공연을 펼칩니다.\n'
+  String get introduction_screen_letter_page_letter => 'Flutter로 세상을 그려내는 개발자입니다.\n\n'
+      '코드 한 줄에 감성을 담아 iOS와 Android라는 두 개의 무대 위에 하나의 아름다운 공연을 펼칩니다.\n\n'
       '새로운 기술과 도전을 사랑하며, 매일 더 나은 소프트웨어라는 꿈을 꿉니다.';
 
 }
@@ -49,8 +49,8 @@ class EnglishLocalizations implements Localizations{
 
   @override
   // TODO: implement introduction_screen_letter_page_letter
-  String get introduction_screen_letter_page_letter => 'I am a developer painting the world with Flutter.\n'
-      'Each line of code carries a touch of emotion, crafting a single beautiful performance on the dual stages of iOS and Android. \n'
+  String get introduction_screen_letter_page_letter => 'I am a developer painting the world with Flutter.\n\n'
+      'Each line of code carries a touch of emotion, crafting a single beautiful performance on the dual stages of iOS and Android. \n\n'
       'I embrace new technologies and challenges, dreaming each day of creating better software.';
 
 
--- lib/ui/screens/introduction/pages/cover_letter_page.dart
+++ lib/ui/screens/introduction/pages/cover_letter_page.dart
@@ -32,74 +32,76 @@ class _CoverLetterPageState extends ConsumerState<CoverLetterPage> {
         color: context.colorScheme.onSurface.withOpacity(0.5));
 
     final TextStyle letterTextStyle = TextStyle(
-        fontWeight: FontWeight.w300,
+        fontWeight: FontWeight.w700,
         fontSize: context.getResponsiveValue(16, 12),
-        color: context.colorScheme.onSurface);
+        color: context.colorScheme.onSurface.withOpacity(0.8));
 
     return FittedScreenSizeBody(
       body: Padding(
         padding: EdgeInsets.all(context.getResponsiveValue(70, 30)),
-        child: Center(
-          child: MaxWidthBox(
-            child: GlassyContainer(
-              height: double.infinity,
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 15),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  MaxWidthBox(
-                    maxWidth: 700,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        CircleAvatar(
-                          minRadius: context.getResponsiveValue(60, 45),
-                          backgroundColor: Colors.white24,
-                        ),
-                        DefaultTextStyle(
-                          style: profileStateTextStyle,
-                          child: const Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Name : Kim Jeong Woo'),
-                              SizedBox(
-                                height: 12,
-                              ),
-                              Text('Age : 26'),
-                              SizedBox(
-                                height: 12,
-                              ),
-                              Text('Job : Flutter software developer'),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Text(
-                    '“',
-                    style: doubleQuotationTextStyle,
-                  ),
-                  MaxWidthBox(
-                    maxWidth: 700,
-                    child: Text.rich(TextSpan(children: [
-                      TextSpan(
-                          text: ref.localizations
-                              .introduction_screen_letter_page_letter,
-                          style: letterTextStyle)
-                    ]),textAlign: TextAlign.center,),
-                  ),
-                  Text(
-                    '”',
-                    style: doubleQuotationTextStyle,
-                  ),
-                ],
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              '“',
+              style: doubleQuotationTextStyle,
+            ),
+            MaxWidthBox(
+              maxWidth: 700,
+              child: Text.rich(
+                TextSpan(children: [
+                  TextSpan(
+                      text: ref
+                          .localizations.introduction_screen_letter_page_letter,
+                      style: letterTextStyle)
+                ]),
+                textAlign: TextAlign.center,
               ),
             ),
-          ),
+
+            Text(
+              '”',
+              style: doubleQuotationTextStyle,
+            ),
+            GlassyContainer(
+              height: context.getResponsiveValue(200, 150),
+              width: context.getResponsiveValue(500, 400),
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+              child: MaxWidthBox(
+                maxWidth: 700,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircleAvatar(
+                      minRadius: context.getResponsiveValue(60, 45),
+                      backgroundColor: Colors.white24,
+                    ),
+                    DefaultTextStyle(
+                      style: profileStateTextStyle,
+                      child: const Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Name : Kim Jeong Woo'),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text('Age : 26'),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text('Job : Flutter software developer'),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
