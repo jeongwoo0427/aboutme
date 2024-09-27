89af502681ab35af3f0c22c4c18094291b6a8845
BamBoo <64342840+jeongwoo0427@users.noreply.github.com>
2024-09-27 17:36:57 +0900

    feat : 로딩 애니메이션을 위해 애니메이션  패키지 설치

---
+++ lib/ui/widgets/load_and_result_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoadAndResultWidget extends StatefulWidget {
+  const LoadAndResultWidget({Key? key}) : super(key: key);
+
+  @override
+  State<LoadAndResultWidget> createState() => _LoadAndResultWidgetState();
+}
+
+class _LoadAndResultWidgetState extends State<LoadAndResultWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
