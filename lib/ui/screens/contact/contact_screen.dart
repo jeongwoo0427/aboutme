7c667635e689b603e19b3aba9f57e0070e987256
BamBoo <64342840+jeongwoo0427@users.noreply.github.com>
2024-09-27 16:17:21 +0900

    feat : 페이드 애니메이션 효과 적용

--- lib/ui/screens/contact/contact_screen.dart
+++ lib/ui/screens/contact/contact_screen.dart
@@ -1,3 +1,4 @@
+import 'package:aboutme/app_router.dart';
 import 'package:aboutme/cores/extensions/widget_ref_extension.dart';
 import 'package:aboutme/ui/widgets/boxes/max_width_box.dart';
 import 'package:aboutme/ui/widgets/buttons/rounded_flat_button.dart';
@@ -5,6 +6,7 @@ import 'package:aboutme/ui/widgets/container/glassy_container.dart';
 import 'package:aboutme/ui/widgets/scaffold/responsive_glassy_scaffold.dart';
 import 'package:aboutme/ui/widgets/textfields/rounded_textfield_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ContactScreen extends ConsumerStatefulWidget {
@@ -16,7 +18,18 @@ class ContactScreen extends ConsumerStatefulWidget {
   ConsumerState<ContactScreen> createState() => _ContactScreenState();
 }
 
-class _ContactScreenState extends ConsumerState<ContactScreen> {
+class _ContactScreenState extends ConsumerState<ContactScreen> with TickerProviderStateMixin {
+  late final AnimationController _fadeInController;
+  late final AnimationController _fadeOutController;
+
+  @override
+  void initState() {
+    _fadeInController = AnimationController(vsync: this);
+    _fadeOutController = AnimationController(vsync: this);
+    Future.delayed(const Duration(milliseconds: 1000),()=>_playFadeIn());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ResponsiveGlassyScaffold(
@@ -26,36 +39,78 @@ class _ContactScreenState extends ConsumerState<ContactScreen> {
         maxHeight: 400,
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: GlassyContainer(
-            width: double.infinity,
-            height: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: RoundedTextFieldWidget(
-                    label: ref.localizations.contact_message_input_label,
-                    expands: true,
-                  ),
-                ),
-                SizedBox(height: 10),
+          child: _Form(
+            onPressedSend: _onPressedSend,
+          ).animate(controller: _fadeInController, autoPlay: false, effects: [
+            const FadeEffect(curve: Curves.decelerate, begin: 0, end: 1, duration: Duration(milliseconds: 1000)),
+            const MoveEffect(duration: Duration(milliseconds: 1000), curve: Curves.decelerate, begin: Offset(-200, 0), end: Offset(0, 0))
+          ]).animate(controller: _fadeOutController, autoPlay: false, effects: [
+            const FadeEffect(curve: Curves.ease, begin: 1, end: 0, duration: Duration(milliseconds: 1000)),
+            const MoveEffect(duration: Duration(milliseconds: 1000), curve: Curves.decelerate, begin: Offset(0, 0), end: Offset(300, 0))
+          ]),
+        ),
+      ),
+    );
+  }
 
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Expanded(child: RoundedTextFieldWidget(label: ref.localizations.contact_mycontact_input_label,)),
-                    const SizedBox(width: 10),
-                    RoundedFlatButton(
-                        padding: EdgeInsets.symmetric(horizontal: 15,vertical: 20),
-                        onPressed: (){}, child: Text('Send')),
-                  ],
-                )
-              ],
+  void _playFadeIn() {
+    _fadeOutController.reset();
+    _fadeInController.reset();
+    _fadeInController.forward();
+  }
+
+  void _playFadeOut() {
+    _fadeOutController.reset();
+    _fadeOutController.forward();
+  }
+
+  void _onPressedSend(String message, String myContact) {
+    print(message);
+    print(myContact);
+    _playFadeOut();
+    Future.delayed(const Duration(milliseconds: 2000),()=>_playFadeIn());
+  }
+}
+
+class _Form extends ConsumerWidget {
+  final Function(String message, String myContact) onPressedSend;
+
+  const _Form({super.key, required this.onPressedSend});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GlassyContainer(
+      width: double.infinity,
+      height: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: RoundedTextFieldWidget(
+              label: ref.localizations.contact_message_input_label,
+              expands: true,
             ),
           ),
-        ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                  child: RoundedTextFieldWidget(
+                label: ref.localizations.contact_mycontact_input_label,
+              )),
+              const SizedBox(width: 10),
+              RoundedFlatButton(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  onPressed: () {
+                    onPressedSend('a', 'b');
+                  },
+                  child: Text('Send')),
+            ],
+          )
+        ],
       ),
     );
   }
