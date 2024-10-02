29926ccfb9a012521fffc6f86aa20110f5e97df6
BamBoo <64342840+jeongwoo0427@users.noreply.github.com>
2024-10-02 11:46:20 +0900

    fix : SkillDetailScreen to SkillDetailPage

---
+++ lib/ui/screens/skills/skill_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SkillDetailPage extends StatefulWidget {
+  const SkillDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<SkillDetailPage> createState() => _SkillDetailPageState();
+}
+
+class _SkillDetailPageState extends State<SkillDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/ui/screens/skills/skills_screen.dart
+++ lib/ui/screens/skills/skills_screen.dart
@@ -1,4 +1,4 @@
-import 'package:aboutme/ui/screens/skills/skill_detail_screen.dart';
+import 'package:aboutme/ui/screens/skills/skill_detail_page.dart';
 import 'package:aboutme/ui/screens/skills/skill_total_page.dart';
 import 'package:flutter/material.dart';
 import '../../widgets/scaffold/responsive_glassy_scaffold.dart';
