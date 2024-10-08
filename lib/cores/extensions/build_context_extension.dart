3a791b6d713b84b4c7ebf2b2cc65a3c89db08519
BamBoo <64342840+jeongwoo0427@users.noreply.github.com>
2024-10-08 09:36:04 +0900

    Update build_context_extension.dart

--- lib/cores/extensions/build_context_extension.dart
+++ lib/cores/extensions/build_context_extension.dart
@@ -6,7 +6,7 @@ import 'package:responsive_framework/responsive_framework.dart';
 extension BuildContextExtension on BuildContext{
   ColorScheme get colorScheme => Theme.of(this).colorScheme;
   TextTheme get textTheme => Theme.of(this).textTheme;
-  Size get screenSize => MediaQuery.of(this).size;
+  Size get screenSize => MediaQuery.sizeOf(this);
   T getResponsiveValue<T>(T defaultValue, T whenSmallerThanTabletValue){
     return ResponsiveValue<T>(this,defaultValue:defaultValue,conditionalValues: [
       Condition.smallerThan(name: TABLET, value: whenSmallerThanTabletValue)
