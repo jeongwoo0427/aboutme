6f82079b5e2914fb0343eebbb59f656467eebff4
JJeong Kim <64342840+jeongwoo0427@users.noreply.github.com>
2024-10-11 01:25:28 +0900

    feat : 스킬정보 컨테이너 디자인 적용 완료

--- lib/ui/screens/skills/skill_detail_page.dart
+++ lib/ui/screens/skills/skill_detail_page.dart
@@ -16,14 +16,20 @@ class SkillDetailPage extends StatefulWidget {
   State<SkillDetailPage> createState() => _SkillDetailPageState();
 }
 
-class _SkillDetailPageState extends State<SkillDetailPage> with TickerProviderStateMixin {
+class _SkillDetailPageState extends State<SkillDetailPage>
+    with TickerProviderStateMixin {
   late final Animation<double> _circleContainerAnimation;
   late final AnimationController _circleContainerAnimationController;
 
   late final Animation<double> _showInfoAnimation;
   late final AnimationController _showInfoAnimationController;
 
-
+  @override
+  void dispose() {
+    _circleContainerAnimationController.dispose();
+    _showInfoAnimationController.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
@@ -39,91 +45,147 @@ class _SkillDetailPageState extends State<SkillDetailPage> with TickerProviderSt
 
     return AnimatedBuilder(
       animation: _showInfoAnimation,
-        builder: (context,child){
+      builder: (context, child) {
         final double value = _showInfoAnimation.value;
-          return Stack(
-            children: [
-              Positioned(
-                left: 0 + value*250*sizeRatio,
-                right: 0,
-                top: 0,
-                bottom: 0,
-                child: Center(
-                    child: MaxSizedBox(
-                      maxWidth: 400,
-                      maxHeight: 600,
-                      child: Opacity(
-                        opacity: value,
-                        child: GlassyContainer(
-                          width: 500*sizeRatio,
-                          height: 800*sizeRatio,
-                          child: Text(''),
-                        ),
-                      ),
-                    )),
-              ),
-              Positioned(
-                left: 0,
-                right: 0 + value*550*sizeRatio,
-                top: 0,
-                bottom: 0,
-                child: Stack(
-                  children: [
-                    Positioned.fill(
-                      child: Center(
-                        child: AnimatedBuilder(
-                          animation: _circleContainerAnimation,
-                          builder: (context, child) {
-                            return Transform.scale(
-                              scale: _circleContainerAnimation.value,
-                              child: Opacity(opacity: _circleContainerAnimation.value, child: child),
-                            );
-                          },
-                          child: Container(
-                            width: 150 * sizeRatio,
-                            height: 150 * sizeRatio,
-                            decoration: BoxDecoration(
-                                border: Border.all(color: context.colorScheme.onSurface.withOpacity(0.8), width: 2 * context.screenSizeRatio),
-                                borderRadius: BorderRadius.circular(2000)),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Positioned.fill(
-                      child: Center(
-                        child: SizedBox(
-                          height: 80 * sizeRatio,
-                          width: 80 * sizeRatio,
-                          child: Hero(
-                            tag: 'SkillIconHero-${skillModel.name}',
-                            child: SvgPicture.asset(
-                              skillModel.iconAsset,
-                              colorFilter: ColorFilter.mode(context.colorScheme.onSurface, BlendMode.srcIn),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+        return Stack(
+          children: [
+            Positioned(
+              left: 0 + value * 250 * sizeRatio,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Center(
+                  child: MaxSizedBox(
+                maxWidth: 400,
+                maxHeight: 600,
+                child: Opacity(
+                  opacity: value,
+                  child: GlassyContainer(
+                    width: 500 * sizeRatio,
+                    height: 800 * sizeRatio,
+                    padding: EdgeInsets.all(context.getResponsiveValue(25, 15)),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                            flex: 2,
+                            child: Center(
+                              child: Text(
+                                skillModel.name,
+                                style: TextStyle(
+                                    fontSize:
+                                        context.getResponsiveValue(38, 24),
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            )),
 
-            ],
-          );
-        },
+                        Expanded(
+                            flex: 5,
+                            child: Text(
+                              'this is my lovely stack and i love it so much so i will be good developer at it\ndkdkkfj\ndfkewrerwerwerwerwerewrwerwerwrwerwer',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                  fontSize:
+                                  context.getResponsiveValue(20, 14),
+                                  fontWeight: FontWeight.w300),
+                            )),
+                        Expanded(
+                            flex: 1,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  'Started at 23. 02. 09',
+                                  textAlign: TextAlign.start,
+                                  style: TextStyle(
+                                      fontSize:
+                                      context.getResponsiveValue(23, 16),
+                                      fontWeight: FontWeight.w500),
+                                )
+                              ],
+                            )),
+                      ],
+                    ),
+                  ),
+                ),
+              )),
+            ),
+            Positioned(
+              left: 0,
+              right: 0 + value * 550 * sizeRatio,
+              top: 0,
+              bottom: 0,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Center(
+                      child: AnimatedBuilder(
+                        animation: _circleContainerAnimation,
+                        builder: (context, child) {
+                          return Transform.scale(
+                            scale: _circleContainerAnimation.value,
+                            child: Opacity(
+                                opacity: _circleContainerAnimation.value,
+                                child: child),
+                          );
+                        },
+                        child: Container(
+                          width: 150 * sizeRatio,
+                          height: 150 * sizeRatio,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: context.colorScheme.onSurface
+                                      .withOpacity(0.8),
+                                  width: 1.5),
+                              borderRadius: BorderRadius.circular(2000)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Center(
+                      child: SizedBox(
+                        height: 80 * sizeRatio,
+                        width: 80 * sizeRatio,
+                        child: Hero(
+                          tag: 'SkillIconHero-${skillModel.name}',
+                          child: SvgPicture.asset(
+                            skillModel.iconAsset,
+                            colorFilter: ColorFilter.mode(
+                                context.colorScheme.onSurface, BlendMode.srcIn),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 
   void initAnimations() {
-    _circleContainerAnimationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 1000));
-    _circleContainerAnimation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(parent: _circleContainerAnimationController, curve: Curves.decelerate));
+    _circleContainerAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+    _circleContainerAnimation = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(
+            parent: _circleContainerAnimationController,
+            curve: Curves.decelerate));
 
-    _showInfoAnimationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 1000));
-    _showInfoAnimation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(parent: _showInfoAnimationController, curve: Curves.decelerate));
+    _showInfoAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+    _showInfoAnimation = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(
+            parent: _showInfoAnimationController, curve: Curves.decelerate));
 
-
-
-    Future.delayed(const Duration(milliseconds: 600), () => {_circleContainerAnimationController.forward()});
-    Future.delayed(const Duration(milliseconds: 1600), () => {_showInfoAnimationController.forward()});
+    Future.delayed(const Duration(milliseconds: 600),
+        () => {_circleContainerAnimationController.forward()});
+    Future.delayed(const Duration(milliseconds: 1600),
+        () => {_showInfoAnimationController.forward()});
   }
 }
