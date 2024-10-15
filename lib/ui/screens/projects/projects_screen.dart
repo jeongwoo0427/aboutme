f027ea2045e9166e0c3bdbfb9dc000aa05d105ed
BamBoo <64342840+jeongwoo0427@users.noreply.github.com>
2024-10-15 17:28:37 +0900

    feat : 프로젝트 화면 작업 진행중

--- lib/ui/screens/projects/projects_screen.dart
+++ lib/ui/screens/projects/projects_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:aboutme/cores/extensions/build_context_extension.dart';
 import 'package:flutter/material.dart';
 import '../../widgets/scaffold/responsive_glassy_scaffold.dart';
 
@@ -11,12 +15,35 @@ class ProjectsScreen extends StatefulWidget {
 }
 
 class _ProjectsScreenState extends State<ProjectsScreen> {
+  final PageController _pageController = PageController(viewportFraction: 0.2);
 
   @override
   Widget build(BuildContext context) {
+
     return ResponsiveGlassyScaffold(
+      isTranparentAppbar: false,
       appbarTitle: Text('Projects'),
-      body: Text(''),
+      body: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {PointerDeviceKind.touch, PointerDeviceKind.mouse}),
+        child: PageView.builder(
+          physics: PageScrollPhysics(),
+          controller: _pageController,
+          itemBuilder: (context, index) {
+            return getItem(width: context.screenSize.width/10, height: context.screenSize.width/10);
+          },
+          itemCount: 10,
+
+        ),
+      ),
     );
+  }
+
+  getItem({double width = 200, double height = 200}) {
+    return Center(
+        child: Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(color: Colors.red),
+    ));
   }
 }
