77b36e013e40de91bf5833c97094f0f931df8771
BamBoo <64342840+jeongwoo0427@users.noreply.github.com>
2024-10-16 16:17:40 +0900

    feat : project 화면 페이지 등장 애니메이션 적용 완료

--- lib/ui/screens/projects/projects_screen.dart
+++ lib/ui/screens/projects/projects_screen.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
 import 'dart:ui';
-
 import 'package:aboutme/cores/extensions/build_context_extension.dart';
+import 'package:aboutme/ui/widgets/container/glassy_container.dart';
 import 'package:flutter/material.dart';
 import '../../widgets/scaffold/responsive_glassy_scaffold.dart';
 
@@ -14,36 +13,90 @@ class ProjectsScreen extends StatefulWidget {
   State<ProjectsScreen> createState() => _ProjectsScreenState();
 }
 
-class _ProjectsScreenState extends State<ProjectsScreen> {
-  final PageController _pageController = PageController(viewportFraction: 0.2);
+class _ProjectsScreenState extends State<ProjectsScreen> with TickerProviderStateMixin {
+  late final AnimationController _pageLoadAnimController;
+  late final Animation _pageLoadAnim;
+
+  late final AnimationController _pageToBottomAnimController;
+  late final Animation _pageToBottomAnim;
+
+  final PageController _pageController = PageController(viewportFraction: 0.3, initialPage: 9);
+
+  @override
+  void dispose() {
+    _pageLoadAnimController.dispose();
+    _pageToBottomAnimController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _initAnimations();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return ResponsiveGlassyScaffold(
       isTranparentAppbar: false,
       appbarTitle: Text('Projects'),
-      body: ScrollConfiguration(
-        behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {PointerDeviceKind.touch, PointerDeviceKind.mouse}),
-        child: PageView.builder(
-          physics: PageScrollPhysics(),
-          controller: _pageController,
-          itemBuilder: (context, index) {
-            return getItem(width: context.screenSize.width/10, height: context.screenSize.width/10);
-          },
-          itemCount: 10,
-
-        ),
+      body: Stack(
+        children: [
+          AnimatedBuilder(
+            animation: _pageToBottomAnim,
+            builder: (context, child) {
+              return AnimatedBuilder(
+                animation: _pageLoadAnim,
+                builder: (_, __) {
+                  return Positioned(
+                    left: (-context.screenSize.width / 2) * _pageLoadAnim.value,
+                    right: (context.screenSize.width / 2) * _pageLoadAnim.value,
+                    top: context.screenSize.height / 1.5 * _pageToBottomAnim.value,
+                    bottom: 0,
+                    child: Center(
+                      child: SizedBox(
+                        height: 150 * context.minScreenLengthRatio,
+                        child: ScrollConfiguration(
+                          behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {PointerDeviceKind.touch, PointerDeviceKind.mouse}),
+                          child: PageView.builder(
+                            controller: _pageController,
+                            itemBuilder: (context, index) {
+                              return Center(
+                                child: GlassyContainer(
+                                  width: 250 * context.minScreenLengthRatio,
+                                  height: double.infinity,
+                                  child: Text('hi'),
+                                ),
+                              );
+                              //return getItem(width: 250 * context.minScreenLengthRatio, height: double.infinity);
+                            },
+                            itemCount: 10,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          )
+        ],
       ),
     );
   }
 
-  getItem({double width = 200, double height = 200}) {
-    return Center(
-        child: Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(color: Colors.red),
-    ));
+  Future<void> _initAnimations() async {
+    _pageLoadAnimController = AnimationController(vsync: this, duration: const Duration(milliseconds: 300));
+    _pageLoadAnim = Tween<double>(begin: 1, end: 0).animate(_pageLoadAnimController);
+
+    _pageToBottomAnimController = AnimationController(vsync: this, duration: const Duration(milliseconds: 800));
+    _pageToBottomAnim = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(parent: _pageToBottomAnimController, curve: Curves.ease));
+
+    await Future.delayed(const Duration(milliseconds: 300));
+
+    await _pageLoadAnimController.forward();
+    await _pageController.animateToPage(0, duration: const Duration(milliseconds: 3000), curve: Curves.decelerate);
+    await _pageToBottomAnimController.forward();
   }
 }
